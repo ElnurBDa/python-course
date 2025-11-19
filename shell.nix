8e42cb905a040af9b9c965d62433feb3e0691194
ElnurBDa <elnurbda@gmail.com>
2025-11-19 20:59:47 +0400

    init

---
+++ shell.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    # PDF tools
+    pdftk
+    poppler_utils
+
+    # Python with WeasyPrint
+    (python313.withPackages (ps: [
+      ps.weasyprint
+    ]))
+  ];
+
+  shellHook = ''
+    echo "✅ Development shell ready."
+    echo "Use 'pdftk --version' to confirm it's installed."
+  '';
+}
+
